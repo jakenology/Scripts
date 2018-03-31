ddde97ed916a7af9b57e52f1ca91c907c7885578
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-03-31 04:18:36 -0500

    Version 1.1

--- reset-dock.sh
+++ reset-dock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Reset Dock Version 1.0
+## Reset Dock Version 1.1
 ## Created and tested by Jayke Peters on 03/23/2018. 
 ## Deployable via macOS Server, Centrify, JumpCloud, Casper Suite/Jamf Pro as a Policy and other MDM solutions.
 # Note that the ".sh" file extension is not needed as long as the shebang is on top (the interpreter), however it is needed in all JAMF solutions.
@@ -11,7 +11,7 @@
 currentuser=`stat -f "%Su" /dev/console`
 
 # Reset the Dock
-/usr/bin/sudo -u $currentuser /usr/bin/defaults delete com.apple.dock; killall Dock
+/usr/bin/sudo -u $currentuser /usr/bin/defaults delete com.apple.dock
 
 # Refresh the Dock
 /usr/bin/killall Dock
