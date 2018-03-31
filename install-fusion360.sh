cb1ad21c8dde363a627a5084838a55964a51e415
jaykepeters <37952082+jaykepeters@users.noreply.github.com>
2018-03-31 03:44:12 -0500

    Add files via upload

---
+++ install-fusion360.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Kill all open Autodesk Applications
+pkill Fusion 360*
+sleep 5
+pkill Autodesk*
+sleep 5
+
+# Download the DMG from Autodesk
+curl -Lo /tmp/fusion.dmg https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Client%20Downloader.dmg --silent
+
+# Mount the Volume
+hdiutil attach -nobrowse -noverify -notremovable /tmp/fusion.dmg > /dev/null
+
+# Perform the Silent Install
+/Volumes/Autodesk\ Client\ Downloader/Right-click\ \>\ Open\ to\ Install.app/Contents/MacOS/streamer -p deploy --quiet
+
+# Unmount the Volume
+hdiutil detach /Volumes/Autodesk\ Client\ Downloader > /dev/null
+
+# Remove the DMG
+rm /tmp/fusion.dmg
