b989519a4b7b31805614f289bbddc858baa0e6d1
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-04-11 19:13:19 -0500

    Version 1.2.0

--- install-screen_savers.sh
+++ install-screen_savers.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-## Quick Execution
-curl -fkL curl -fkL https://raw.githubusercontent.com/jaykepeters/Scripts/master/install-screen_savers.sh | sh
-
 ## Set Variables
 DESTINATION=~/Library/Screen\ Savers
 
@@ -34,6 +31,12 @@ rm -R "/tmp/__MACOSX"
 osascript -e 'tell application "System Events"
 	display dialog "Your Screen Savers have been successfully installed." buttons {"Thanks!"} default button {"Thanks!"} with icon {"/tmp/success.pdf"} giving up after 3
 end tell' > /dev/null
+
+## Open the Screensaver Pane
+osascript -e 'tell application "System Preferences"
+	activate
+	reveal anchor "ScreenSaverPref" of pane "com.apple.preference.desktopscreeneffect"
+end tell'
 
 ## Remove the success icon
 rm -R /tmp/Success.pdf
