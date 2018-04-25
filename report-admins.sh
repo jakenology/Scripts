19d9087c5c90ac6873507ed977695cd1e4f1a2e3
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-04-24 20:35:17 -0500

    Report Local Admins - v0

---
+++ report-admins.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script to detect if a computer has a local admin account on it with an UID of above 500
+
+# Initialize array
+
+list=()
+
+
+# generate user list of users with UID greater than 500
+
+for username in $(dscl . list /Users UniqueID | awk '$2 > 500 { print $1 }'); do
+
+# Checks to see which usernames are reported as being admins. The
+# check is running dsmemberutil's check membership and listing the
+# accounts that are being reported as admin users. Actual check is
+# for accounts that are NOT not an admin (i.e. not standard users.)
+
+    if [[ $(dsmemberutil checkmembership -U "${username}" -G admin) != *not* ]]; then
+    # Any reported accounts are added to the array list
+        list+=("${username}")
+    fi
+done
+
+# Print to the user the title
+echo "LOCAL ADMIN USERNAME's:"
+
+# Prints the array's list contents
+for uzer in ${list[@]}; do
+    echo $uzer
+done
+
