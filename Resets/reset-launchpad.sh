3f955aaf36aa77536eb284bf9e70700c357bd2aa
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-06-16 03:48:56 -0500

    Update reset-launchpad.sh

--- Resets/reset-launchpad.sh
+++ Resets/reset-launchpad.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-## Reset Launchpad Version 1.0
-## Created and tested by Jayke Peters on 03/23/2018. 
+## Reset Launchpad Version 1.0.1
+## VERSION 1 Created and tested by Jayke Peters on 03/23/2018. 
+## VERSION 1.0.1:         DESCRIPTION CHANGED BY JP on 6/16/18
 ## Deployable via macOS Server, Centrify, JumpCloud, Casper Suite/Jamf Pro as a Policy and other MDM solutions.
-# Note that the ".sh" file extension is not needed as long as the shebang is on top (the interpreter).
-# Using the which command, the paths were specified as this script may fail when deployed via Casper Suite/Jamf Pro.
-# In the event that this script fails via Self Service, you may need to find another way to run it as the logged in user via Jamf Nation.
+# Using the which command, the paths were specified as this script may fail when deployed via JumpCloud and Jamf Pro. 
+## Jamf Pro JSS and Jamf Agent may set the $1, $2, $3 variables upon script deployment. This was tested and certified to work on JumpCloud by JP.
+## ZOHO Desktop Central Depoloyment testing via server. Batch and Bash scripts ready.. Internal trial instance set to port 8888.
 
 # Store the current user's username as a variable
 currentuser=`stat -f "%Su" /dev/console`
