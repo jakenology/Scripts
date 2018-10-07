f765175f4b722222fa1c39f438b6208e6ff0e2df
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-10-06 19:13:49 -0500

    Update reset-dock.sh

--- Resets/reset-dock.sh
+++ Resets/reset-dock.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-## Reset Dock Version 1.1
+## Reset Dock Version 1.2
 ## Created and tested by Jayke Peters on 03/23/2018. 
-## Deployable via macOS Server, Centrify, JumpCloud, Casper Suite/Jamf Pro as a Policy and other MDM solutions.
-# Note that the ".sh" file extension is not needed as long as the shebang is on top (the interpreter), however it is needed in all JAMF solutions.
-# Using the which command, the paths were specified as this script may fail when deployed via Casper Suite/Jamf Pro.
-# In the event that this script fails via Self Service, you may need to find another way to run it as the logged in user via Jamf Nation.
+## Modified Apr 26
+## Modified October 6
+## Deployable via Centrify, JumpCloud, Jamf Pro and other solutions.
 
 # Store the current user's username as a variable
 currentuser=`stat -f "%Su" /dev/console`
