7f05beb7bd005fe2d19a98bad296b1549bedae80
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-10-06 19:16:10 -0500

    Update reset-launchpad.sh

--- Resets/reset-launchpad.sh
+++ Resets/reset-launchpad.sh
@@ -3,10 +3,9 @@
 ## Reset Launchpad Version 1.0.1
 ## VERSION 1 Created and tested by Jayke Peters on 03/23/2018. 
 ## VERSION 1.0.1:         DESCRIPTION CHANGED BY JP on 6/16/18
-## Deployable via Apple Remote Desktop, Centrify, JumpCloud, Casper Suite/Jamf Pro as a Policy and other MDM solutions.
-# Using the which command, the paths were specified as this script may fail when deployed via JumpCloud and Jamf Pro. 
-## Jamf Pro JSS and Jamf Agent may set the $1, $2, $3 variables upon script deployment. This was tested and certified to work on JumpCloud by JP.
-## ZOHO Desktop Central Depoloyment testing via server. Batch and Bash scripts ready.. Internal trial instance set to port 8888.
+## VERSION 1.0.2          DESCRIPTION CHANGED BY JP on 10/6/18
+## Deployable via Centrify, JumpCloud, Jamf Pro and other solutions.
+## Jamf Pro JSS supposedly sets the $1, $2, $3 parameters upon script deployment.
 
 # Store the current user's username as a variable
 currentuser=`stat -f "%Su" /dev/console`
