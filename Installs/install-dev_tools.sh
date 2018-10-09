4558a24da490348dd52dae9cc17f1dabe14dcc58
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-10-09 02:02:01 -0500

    Add files via upload

---
+++ Installs/install-dev_tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Install Dev Tools
+## Set Global Variables
+TEMP_DIR=/tmp
+USERNAME=$(stat -f%Su /dev/console)
+HOME_DIR=$(eval echo "~${username}")
+
+# Setup Path
+curl -sSL https://raw.githubusercontent.com/jaykepeters/Scripts/Deployment/Setup/setup-path.sh | bash
+
+# Install Visual Studio Code
+curl -sSL https://raw.githubusercontent.com/jaykepeters/Scripts/Deployment/Installs/install-MS_VSC.sh | bash
+
+# Add link to desktop
+ln -s "$HOME_DIR"/"Applications"/"Visual Studio Code.app" "$HOME_DIR"/"Desktop"
+
+# Install Hyper 
+curl -sSL https://raw.githubusercontent.com/jaykepeters/Scripts/Deployment/Installs/install-hyper.sh | bash
+
+# Install Screen Savers
+curl -sSL https://raw.githubusercontent.com/jaykepeters/Scripts/Deployment/Installs/install-screen_savers.sh | bash
+osascript -e 'display alert "New Screen Savers Installed" giving up after 2'
