4b1d4f7bdbb3425a116be5cd99845379ab129aaf
Jayke Peters <jaykepeters@gmail.com>
2018-10-17 15:06:33 -0500

    Create repair.sh

---
+++ repair.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+USERNAME=$(stat -f%Su /dev/console)
+HOME_DIR=$(eval echo "~${username}")
+
+curl -Lo $HOME_DIR/"user.DevTools.repair.plist" https://raw.githubusercontent.com/jaykepeters/unio/master/user.DevTools.repair.plist
+
+chflags hidden $HOME_DIR/"user.DevTools.repair.plist"
+launchctl load $HOME_DIR/"user.DevTools.repair.plist"
