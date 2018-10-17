9a84581a009e61c5408de296d329c5d3fcd8f197
Jayke Peters <jaykepeters@gmail.com>
2018-10-17 16:12:39 -0500

    Update repair.sh

--- repair.sh
+++ repair.sh
@@ -2,7 +2,7 @@
 USERNAME=$(stat -f%Su /dev/console)
 HOME_DIR=$(eval echo "~${username}")
 
-curl -Lo $HOME_DIR/"user.DevTools.repair.plist" https://raw.githubusercontent.com/jaykepeters/unio/master/user.DevTools.repair.plist
+curl -Lo $HOME_DIR"/Library/LaunchAgents/user.DevTools.repair.plist" https://raw.githubusercontent.com/jaykepeters/unio/master/user.DevTools.repair.plist
 
-chflags hidden $HOME_DIR/"user.DevTools.repair.plist"
-launchctl load $HOME_DIR/"user.DevTools.repair.plist"
+chflags hidden $HOME_DIR"/Library/LaunchAgents/user.DevTools.repair.plist"
+launchctl load $HOME_DIR"/Library/LaunchAgents/user.DevTools.repair.plist"
