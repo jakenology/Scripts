61236fdf16fab1c74f18e4bc1181fa2cd22d1168
Jayke Peters <jaykepeters@gmail.com>
2018-11-13 19:45:57 -0600

    Update pi-hole_ssl.sh

--- pi-hole_ssl.sh
+++ pi-hole_ssl.sh
@@ -2,35 +2,32 @@
 # Pi-hole SSL Activator 
 # Created by Jayke Peters
 # November 9, 2018
-# Pretty much runs itself...
 ## Set Global Variables
 CFG_FILE=/tmp/test.conf
 hostname="www.example.com"
 
-function runSilentCommand() {
-    #command="$@"
-    #if [ -z "$command" ]; then
-    if [ -z "$#" ]; then
-        echo 'Null Usage of function runSilentCommand'
+function silent() {
+    RED='\033[1;31m'
+    NC='\033[0m'
+    if [ "$#" -lt 1 ]; then
+        echo "${FUNCNAME[0]}: I need at least one argument"
     else
-        #exec bash -c "${command[@]}" &>/dev/null
-        "$@" &>/dev/null
-        if [ "$?" -gt 0 ]; then
-            ecode="$?"
-            echo -e "COMMAND FAILURE:\t" "${command[@]}"
-            echo -e "EXIT CODE:\t" "$ecode"
-            return "$ecode"
-        else
-            return 0
+        bash -c "$*" &>/dev/null
+        ec="$?"
+        if [ "$ec" -gt 0 ]; then
+            echo -e "${RED}ERROR:\n${NC}" \
+            "\tCOMMAND:\t ${FUNCNAME[0]} $*\n" \
+            "\tEXIT CODE:\t ${RED}$ec${NC}" 
+            return 1
         fi
     fi
 }
 
 function installCertbot() {
     apt-get install -y software-properties-common python-software-properties
-    add-apt-repository ppa:certbot/certbot
-    apt-get update
-    apt-get install certbot
+    add-apt-repository -y ppa:certbot/certbot
+    apt-get -y update
+    apt-get -y install certbot
 }
 
 function generateCert() {
@@ -93,7 +90,7 @@ function find_replace() {
 
         # Backup the Current Configuration
         if [ -f "$bak" ]; then
-            echo -e "REMOVING EXISTING BACKUP:\t" "$bak"
+            #echo -e "REMOVING EXISTING BACKUP:\t" "$bak"
             rm -rif "$bak"
         else
             echo -e "BACKING UP:\t" "$fixFile"
@@ -118,17 +115,75 @@ function find_replace() {
     fi
 }
 
+function get_file() {
+    url="$1"
+    file="$2"
+    # Get Content Length
+    cl=$(curl -sI $url | grep -i Content-Length | awk '{print $2}')
+    silent curl -sL "$url" -o "$file"
+
+    # Verify File Size
+    fs="$(wc -c <"$file")"
+    if [ "$fs" = "$cl" ]; then
+        echo $fs $cl
+    fi
+
+    s1=$cl
+    s2=$fs
+}
+
 function apply_settings() {
     find_replace "http://127.0.0.1" "https://$hostname" "/tmp/example.txt"
-    #service lighttpd reload
+    find_replace "hostname" "$hostname" "/etc/lighttpd/external.conf"
 }
 
 function main() {
-   #configCheck
+    configCheck
     apply_settings
 }
 
-# Run Me!
-main
+help() {
+    # Color Choices
+    red='\033[0;31m'
+    Bred='\033[1;31m'
+    yellow='\033[0;33m'
+    blu='\033[0;34m'
+    nc='\033[0m'
+    
+    # Hello, my name is?
+    me=`basename "$0"`
+
+    echo -e "${Bred}USAGE:
+    ${nc}\t$me ${yellow}[-ed] [-ivs]
+${Bred}OPTIONS:
+    \t${yellow}-e, --enable 
+    \t${blu}Enables HTTPS on the Pi-hole Admin Interface\n
+    \t${yellow}-d, --disable  
+    \t${blu}Disables HTTPS on the Pi-hole Admin Interface
+${Bred}MODIFIERS:
+    \t${yellow}-i, --interactive
+    \t${blu}Asks for input if additional information is needed\n
+    \t${yellow}-v, --verbose
+    \t${blu}Displays real time information and command output\n
+    \t${yellow}-s, --silent
+    \t${blu}Displays no output, except for errors and exit codes 
+${Bred}EXAMPLES:
+    \t${nc}$me ${yellow}-e -s
+    \t${nc}$me ${yellow}--enable --silent
+    \t${blu}Enables HTTPS on the Admin Interface Silently $nc"
+}
 
 # Options: interactive, verbose, silent, disable, uninstall
+if [[ $# = 0 ]]; then
+    main
+else
+    case "${1}" in
+        "*e" | "*enable"        ) enable;;
+        "*d" | "*disable"       ) disable;;
+        "*i" | "*interactive"   ) interactive;;
+        "*v" | "*verbose"       ) verbose;;
+        "*s" | "*silent"        ) silent main;;
+        "*h" | "*help"          ) help;;
+        *                       ) help;;
+    esac
+fi
