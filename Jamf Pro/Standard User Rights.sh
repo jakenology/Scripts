472677adb51375abe50df49fe883204d9cd14128
Jayke Peters <jaykepeters@gmail.com>
2019-06-03 23:31:00 -0500

    Update Standard User Rights.sh

--- Jamf Pro/Standard User Rights.sh
+++ Jamf Pro/Standard User Rights.sh
@@ -9,23 +9,4 @@
 /usr/bin/security authorizationdb write system.print.operator allow
 /usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group lpadmin
 /usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group _lpadmin 
-/usr/sbin/dseditgroup -o edit -n /Local/Default -a 'Domain Users' -t group lpadmin 
-
-# Kill The Following Applications
-for app in "System Preferences"
-do
-	# Search running processes and find ".app" bundle
-	kill -9 $(pgrep -f "$app.app" | head -n 1)
-
-	# Exact Process Name (user renamed app or not)
-	# Found in app/Contents/Info.plist under CFBundleExecutable
-	# plutil -convert xml1 [path to Info.plist] (binary to xml)
-	# Open in text editor and find CFBundleExecutable
-	# Or you can go in activity monitor and check process name
-	pkill "$app"
-done
-
-# This never happened (Do you want this policy to display as run in JSS and Self Service?)
-#nohup jamf flushPolicyHistory "Standard User Rights" &>/dev/null
-
-exit 0
+/usr/sbin/dseditgroup -o edit -n /Local/Default -a 'Domain Users' -t group lpadmin
