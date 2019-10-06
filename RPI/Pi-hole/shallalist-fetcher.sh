4482559268828aabce0f3fa289355b60a23d311d
Jayke Peters <jaykepeters@gmail.com>
2019-10-06 05:27:49 -0500

    Update shallalist-fetcher.sh

--- RPI/Pi-hole/shallalist-fetcher.sh
+++ RPI/Pi-hole/shallalist-fetcher.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 ## Daily Blocklist Fetcher
 ## SELECTED LISTS
-lists=(
-    "searchengines"
-)
-##
+if [ -f '/etc/shallalists.txt' ]; then
+    lists=($(cat /etc/shallalists.txt))
+else
+    echo -e 'Error:\t No config file!'
+    exit 1
+fi
 shallaurl='http://www.shallalist.de/Downloads/shallalist.tar.gz'
+url='http://pi.hole/shallalist.txt'
+blocklist=/var/www/html/shallalist.txt
 
 # Download list package
 echo -e 'Status:\t Downloading package'
-curl $shallaurl --output /tmp/shallalist.tar.gz
+curl $shallaurl --output /tmp/shallalist.tar.gz --silent
 
 # Extract list package
 echo -e 'Status:\t Extracting package'
-tar -xvf /tmp/shallalist.tar.gz
+tar -xvf /tmp/shallalist.tar.gz > /dev/null 2>&1
 
 # Remove old lists
-echo -e 'Status:\t Removing old lists'
-rm -rf /var/www/html/shallalist.*
+echo -e 'Status:\t Removing old list'
+rm -rf $blocklist > /dev/null 2>&1
 sed -i.bak '/shallalist/d' /etc/pihole/adlists.list
 
 # Extract selected lists
 for list in "${lists[@]}"; do
-    bl="/tmp/BL/$list/domains"
-    url="http://pi.hole/shallalist.$list.txt"
+    bl=/tmp/BL/$list/domains
     
     if [ -f "$bl" ]; then
-        echo -e "Status:\t Moving blacklist $list to webroot"
-        mv $bl /var/www/html/shallalist.$list.txt
-        chown www-data:www-data /var/www/html/shallalist.$list.txt
-
-        echo -e "Status:\t Adding blacklist category $list to Pi-hole"
-        grep -qxF "$url" /etc/pihole/adlists.list || echo "$url" >> /etc/pihole/adlists.list
+        echo -e "Status:\t Adding category $list to master"
+        cat $bl >> $blocklist
     else
         echo -e "Error:\t Category $list does not exist!"
     fi
 done
 
+# Add list to Pi-hole
+echo -e 'Status:\t Adding consolidated list to Pi-hole'
+grep -qxF "$url" /etc/pihole/adlists.list || echo "$url" >> /etc/pihole/adlists.list
+chown www-data:www-data $blocklist
+
 # Remove used files
 echo -e 'Status:\t Removing used files'
-rm -rf /tmp/shallalist.tar.gz /tmp/BL
+rm -rf /tmp/shallalist.tar.gz /tmp/BL > /dev/null 2>&1
 
 # Update gravity
 echo -e 'Status:\t Updating gravity'
-pihole -g
+pihole -g > /dev/null 2>&1
