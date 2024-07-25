033694e2d744f26d021ceb57b5e1218e9a847519
JakeNology <37952082+jakenology@users.noreply.github.com>
2024-07-25 02:30:47 -0500

    Update reset-MS_VSC.sh

--- reset-MS_VSC.sh
+++ reset-MS_VSC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Reset MS VSC (Mac) - v1.2.4
 ## MIT License 
-## Copyright 2018 Jayke Peters
+## Copyright 2018 JakeNology
 
 ## Quick Execution
 # curl -fkl https://raw.githubusercontent.com/jaykepeters/Scripts/master/reset-MS_VSC.sh | sh
