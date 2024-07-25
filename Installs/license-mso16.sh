c27f92468c5eba5df8131c2eb47a502147883463
JakeNology <37952082+jakenology@users.noreply.github.com>
2024-07-25 02:44:02 -0500

    Update license-mso16.sh

--- Installs/license-mso16.sh
+++ Installs/license-mso16.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pkg="/tmp/mso16vl.pkg"
-curl -sLo "$pkg" https://github.com/jaykepeters/MDM/raw/master/macOS/PKG/Microsoft_Office_2016_VL_Serializer_2.0.pkg
+curl -sLo "$pkg" https://github.com/jakenology/MDM/raw/master/macOS/PKG/Microsoft_Office_2016_VL_Serializer_2.0.pkg
 installer -pkg "$pkg" -target "$3"
